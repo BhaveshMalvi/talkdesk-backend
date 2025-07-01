49d5cebae871f3552071ff1750b830286fa4cf8c
BhaveshMalvi <bhaveshkumarmalvi87@gmail.com>
2025-07-01 11:21:32 +0530

    update config file

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,7 @@
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: [process.env.CLIENT_URL,  
+     "https://talkdesk-frontend-git-main-bhaveshmalvis-projects.vercel.app",
+  "https://talkdesk-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT","DELETE"],
   credentials:true
 }
--- index.js
+++ index.js
@@ -52,13 +52,7 @@ const io = new Server(server, { cors: corsOptions });
 app.set("io", io)
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
+
 
 app.use(express.json());
 app.use(cookieParser());
